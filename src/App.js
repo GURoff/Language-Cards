e3aeba9b132f2c467855f776195db6edf0f89eb7
Alexandr <alexandrgurov1@gmail.com>
2022-09-24 18:46:01 +0200

    set by default

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
 import './styles/App.css';
 
 function App() {
-  const [word, setWord] = useState('');
-
-  const changeWord = () => {
-    setWord(prompt("What Is Your Word?"));
-  }
-
-  useEffect(changeWord, []);
   
   return (
     <div className="App">
-      <h1 style={{textAlign: "center"}}>Homepage</h1>
-      <p>Your word is <b>{word}</b>!</p>
-      <button onClick={changeWord}>Change Word</button>
+
     </div>
   );
 }

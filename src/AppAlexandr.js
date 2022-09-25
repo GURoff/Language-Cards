429ae85af2e00e0e7150ab6ef696e328d9fb9caf
Alexandr <alexandrgurov1@gmail.com>
2022-09-25 17:30:40 +0200

    created translate, but with error.

--- src/AppAlexandr.js
+++ src/AppAlexandr.js
@@ -1,5 +1,7 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import './styles/App.css';
+import Axios from 'axios';
+const axios = require('axios').default;
 
 function AppAlexandr() {
   const [word, setWord] = useState('');
@@ -11,6 +13,41 @@ function AppAlexandr() {
     console.log(newWord);
   }
 
+   {/* translator from https://www.youtube.com/watch?v=0vQkTya1Qb4*/}
+    const [options, setOptions] = useState([]);
+    const [from, setFrom] = useState('eng');
+    const [to, setTo] = useState('eng');
+    const [input, setInput] = useState('');
+    const [output, setOutput] = useState('');
+
+    const translate = () => {
+      const params = new URLSearchParams();
+      params.append('q', input);
+      params.append('source', from);
+      params.append('target', to);
+      params.append('api_key', 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx');
+
+      axios.post('https://libretranslate.de/translate', params, {
+        headers: {
+          'accept': 'application/json',
+          'Content-type': 'application/x-www-form-urlencoded',
+        },
+      }).then(res => {
+        // console.log(res.data);
+        setOutput(res.data.translatedText);
+      })
+    };
+
+    useEffect(()=> {
+      axios
+      .get('https://libretranslate.de/languages',
+      {headers: {
+        'accept': 'application/json'}}).then(res => {
+        console.log(res.data);
+        setOptions(res.data);
+      })
+    }, [])  
+
   return (
     <div className="App">
       <h1 style={{textAlign: "center"}}>Homepage</h1>
@@ -21,7 +58,36 @@ function AppAlexandr() {
         onChange = {(e) => setWord(e.target.value)}
       />
       <button onClick={addWord}>Ok</button>
-      <p>You word is <b>{word}</b></p> 
+      <p>You word is <b>{word}</b></p>
+    {/* translator */}
+        <div>
+          From ({from}):
+            <select onChange = {e => setFrom(e.target.value)}>
+              {options.map(opt => <option key = {opt.code} value = {opt.code}>{opt.name}</option>)}
+            </select>
+          To ({to}):
+          <select onChange = {e => setTo(e.target.value)}>
+              {options.map(opt => <option key = {opt.code} value = {opt.code}>{opt.name}</option>)}
+            </select>
+        </div>
+        <div>
+          <textarea 
+            cols = '30' 
+            rows = '1' 
+            onInput = {(e) => setInput(e.target.value)}>
+          </textarea>
+        </div>
+        <div>
+          <textarea 
+            cols = '30' 
+            rows = '1' 
+            value = {output}>
+          </textarea>
+        </div>
+        <div>
+          <button onClick = {e => translate()}>Translate</button>
+        </div>
+    
     </div>
   );
 }

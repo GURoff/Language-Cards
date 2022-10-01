f9d615a17ff9a76e496807e428fb12fa2e96f191
Alexandr <alexandrgurov1@gmail.com>
2022-10-01 20:23:54 +0200

    modified 1.10.2022

--- src/components/UI/Select/MySelect.jsx
+++ src/components/UI/Select/MySelect.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import classes from './MySelect.module.css';
 
-const MySelect = () => {
+const MySelect = ({ options, defaultValue, value, onChange }) => {
     return (
-        <div>
-            
-        </div>
+        <select
+            className={classes.sortDictionary}
+            value={value}
+            onChange={event => onChange(event.target.value)}
+        >
+            <option
+                disabled
+                value=""
+            >
+                {defaultValue}
+            </option>
+            {options.map(option =>
+                <option key={option.value} value={option.value}>
+                    {option.name}
+                </option>
+            )}
+        </select>
     );
 };
 

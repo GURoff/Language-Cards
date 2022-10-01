f689256a2fee32cc9e0e09884e5e00a412326e06
Alexandr <alexandrgurov1@gmail.com>
2022-10-01 20:24:44 +0200

    modified VocabularyList

--- src/components/VocabularyList.jsx
+++ src/components/VocabularyList.jsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import VocabularyItem from './VocabularyItem';
+import MySelect from './UI/Select/MySelect';
 
-const VocabularyList = ({words}) => {
+const VocabularyList = ({ words, remove, selectedSort, setSelectedSort }) => {
+    const [searchQuery, setSearchQuery] = useState('')
     return (
         <div>
             <h1 style={{ textAlign: 'center', color: 'teal' }}>
                 Your personal vocabulary
             </h1>
+            <div>
+                <input
+                    type="text"
+                    value={searchQuery}
+                    onChange={e => setSearchQuery(e.target.value)}
+                    placeholder="Search..."
+                />
+
+                <MySelect
+                    defaultValue="Sort by"
+                    value={selectedSort}
+                    onChange={sort => setSelectedSort(sort)}
+                    options={[
+                        { value: 'ourWord', name: 'base language' },
+                        { value: 'translatedWord', name: 'target language' },
+                    ]}
+                />
+            </div>
             {words.map((word, index) =>
-                <VocabularyItem number = {index + 1} word={word} key={word.id} />
+                <VocabularyItem remove={remove} number={index + 1} word={word} key={word.id} />
             )}
         </div>
     );

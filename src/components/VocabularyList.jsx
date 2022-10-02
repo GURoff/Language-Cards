e3a75f76171b5d1231d50b10f808852868e26da2
Alexandr <alexandrgurov1@gmail.com>
2022-10-02 18:36:58 +0200

    added styles for animation

--- src/components/VocabularyList.jsx
+++ src/components/VocabularyList.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import VocabularyItem from './VocabularyItem';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 const VocabularyList = ({ words, remove }) => {
 
+    if (!words.length) {
+        return (
+            <h1 style={{ textAlign: 'center', color: 'red' }}>
+                Words not found. <p style={{ color: 'teal' }}>Add the word to your own vocabulary.</p></h1>
+        )
+    }
+
     return (
         <div>
-            {words.map((word, index) =>
-                <VocabularyItem number={index + 1} word={word} key={word.id} remove={remove} />
-            )}
+            <TransitionGroup>
+                {words.map((word, index) =>
+                    <CSSTransition
+                        key={word.id}
+                        timeout={500}
+                        classNames="word"
+                    >
+                        <VocabularyItem number={index + 1} word={word} remove={remove} />
+                    </CSSTransition>
+                )}
+            </TransitionGroup>
         </div>
     );
 }

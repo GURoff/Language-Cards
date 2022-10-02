a1d140d00109942c53df9b9195145b7d2a810a07
Alexandr <alexandrgurov1@gmail.com>
2022-10-02 17:39:23 +0200

    added row of search and sort by base word and by target language

--- src/API/Translator.jsx
+++ src/API/Translator.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import MyButton from '../components/UI/Buttons/MyButton';
 import MyInput from '../components/UI/Input/MyInput';
 import MySelect from '../components/UI/Select/MySelect';
@@ -8,7 +8,7 @@ import classes from './Translator.module.css';
 const axios = require('axios').default;
 
 const TranslatorItem = (props) => {
-  /* instructions for translator from https://www.youtube.com/watch?v=0vQkTya1Qb4*/ 
+  /* instructions for translator from https://www.youtube.com/watch?v=0vQkTya1Qb4*/
   const [options, setOptions] = useState([]);
   const [from, setFrom] = useState('en');
   const [to, setTo] = useState('en');
@@ -47,11 +47,25 @@ const TranslatorItem = (props) => {
   }, [])
   //Create vocabulary-----------------------------------------------------------------------
   const [words, setWords] = useState([
-    { id: 1, ourWord: '', translatedWord: '' },
-    { id: 2, ourWord: '', translatedWord: '' },
-    { id: 3, ourWord: '', translatedWord: '' },
+    // { id: 1, ourWord: 'dd', translatedWord: 'hh' },
+    // { id: 2, ourWord: 'ss', translatedWord: 'gg' },
+    // { id: 3, ourWord: 'aa', translatedWord: 'ff' },
   ])
-//Add new word to our vocabulary from API translator
+
+  //Sorted words in vocabulary for future search
+  const sortedWords = useMemo(() => {
+    if (selectedSort) {
+      return [...words].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]))
+    }
+    return words;
+  }, [selectedSort, words]);
+
+  //Sorted and searched words from vocabulary
+  const sortedAndSearchedWords = useMemo(() => {
+    return sortedWords.filter(word => word.input.toLowerCase().includes(searchQuery))
+  }, [searchQuery, sortedWords]);
+
+  //Add new word to our vocabulary from API translator
   const addNewWord = () => {
     const newWord = {
       id: Date.now(),
@@ -62,15 +76,13 @@ const TranslatorItem = (props) => {
     setInput('');
     setOutput('');
   }
-//Function for removing word from our vocabulary 
+  //Function for removing word from our vocabulary 
   const removeWord = (word) => {
     setWords(words.filter(p => p.id !== word.id))
   }
-//Sorting words in the vocabulary by base language and by translated language 
+  //Sorting words in the vocabulary by base language and by translated language 
   const sortVocabularyWords = (sort) => {
     setSelectedSort(sort);
-    console.log(words);
-    setWords([...words].sort((a, b) => a[sort].localeCompare(b[sort])))
   }
 
   return (
@@ -87,14 +99,13 @@ const TranslatorItem = (props) => {
         </select>
       </div>
       <form>
-{/* Input for our base word */}
+        {/* Input for our base word */}
         <MyInput
-          type='text'
           placeholder='enter your word'
           value={input}
           onInput={(e) => {
             if (/[0-9]/.test(e.target.value)) {
-              alert('You cannot enter numbers or space in this field. Click OK and try again')
+              alert('You cannot enter numbers, space and turn on CapsLock in this field. Click OK and try again')
               setInput('');
             } else {
               setInput(e.target.value);
@@ -102,9 +113,8 @@ const TranslatorItem = (props) => {
           }
           }
         />
-{/* Input for our translated word */}
+        {/* Input for our translated word */}
         <MyInput
-          type='text'
           disabled
           placeholder='here is your translate'
           value={output}
@@ -119,14 +129,13 @@ const TranslatorItem = (props) => {
         <h1 style={{ textAlign: 'center', color: 'teal' }}>
           Your personal vocabulary
         </h1>
-{/* Input for search */}
+        {/* Input for search */}
         <MyInput
-          type="text"
           value={searchQuery}
           onChange={e => setSearchQuery(e.target.value)}
           placeholder="Search..."
         />
-{/* Select for sort by... */}
+        {/* Select for sort by... */}
         <MySelect
           defaultValue="Sort by..."
           value={selectedSort}
@@ -137,8 +146,8 @@ const TranslatorItem = (props) => {
           ]}
         />
       </div>
-      {words.length
-        ? <VocabularyList words={words} remove={removeWord} />
+      {sortedAndSearchedWords.length
+        ? <VocabularyList words={sortedAndSearchedWords} remove={removeWord} />
         : <h1 style={{ textAlign: 'center', color: 'red' }}>
           Words not found. <p style={{ color: 'teal' }}>Add the word to your own vocabulary.</p></h1>
       }

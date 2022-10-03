a72bc5247c65bb7bdfbf365fe6310c7731803130
Alexandr <alexandrgurov1@gmail.com>
2022-10-03 14:31:31 +0200

    added styles for search and for sort

--- src/components/WordFilter.jsx
+++ src/components/WordFilter.jsx
@@ -2,20 +2,22 @@ import React from 'react';
 import MyInput from './UI/Input/MyInput';
 import MySelect from './UI/Select/MySelect';
 
-const WordFilter = ({filter, setFilter}) => {
+const WordFilter = ({ filter, setFilter }) => {
     return (
         <div>
             {/* Input for search */}
             <MyInput
+                style={{ margin: '10px 0' }}
                 value={filter.query}
-                onChange={e => setFilter({...filter, query: e.target.value})}
+                onChange={e => setFilter({ ...filter, query: e.target.value })}
                 placeholder="Search..."
             />
             {/* Select for sort by... */}
             <MySelect
+                style={{ margin: '10px 0' }}
                 defaultValue="Sort by..."
                 value={filter.sort}
-                onChange={selectedSort => setFilter({...filter, sort: selectedSort})}
+                onChange={selectedSort => setFilter({ ...filter, sort: selectedSort })}
                 options={[
                     { value: 'input', name: 'base language' },
                     { value: 'output', name: 'target language' },

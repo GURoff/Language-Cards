ccce137b99e66ad0114414fb7eeeb51df4ef390d
Alexandr <alexandrgurov1@gmail.com>
2022-10-04 13:50:54 +0200

    added comment with table for a future

--- src/components/VocabularyItem.jsx
+++ src/components/VocabularyItem.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import classes from './VocabularyItem.module.css';
 
-const VocabularyItem = (props) => {
+const VocabularyItem = (props, words) => {
 
     return (
         <div>
             {/* List of user's words */}
-            <div className={classes.post}>
+            <div className={classes.wordRow}>
+                {/* <table>
+                    <tr>
+                        <th>ID</th>
+                        <th>From</th>
+                        <th>To</th>
+                    </tr>
+                    {words.map((props) => {
+                        return (
+                            <tr key={props.number}>
+                                <td>{props.number}</td>
+                                <td>{props.word.input}</td>
+                                <td>{props.word.output}</td>
+                            </tr>
+                        )
+                    })}
+                </table> */}
+
                 <div>
                     <div className={classes.postWord}>
                         {props.number}. {props.word.input} -

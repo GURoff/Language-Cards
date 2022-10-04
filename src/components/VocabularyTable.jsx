4593209ba8e7e87d50c68f731616bd6331f95cb7
Alexandr <alexandrgurov1@gmail.com>
2022-10-04 22:03:20 +0200

    added a new file for vocabulary Table. ...in progress

---
+++ src/components/VocabularyTable.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
+import '../../node_modules/react-bootstrap-table/css/react-bootstrap-table.css';
+
+class Table1 extends Component {
+    render() {
+      return (
+        <div>
+          <BootstrapTable data={this.props.wordsTable}>
+            
+            <TableHeaderColumn isKey dataField='id'>
+              #
+            </TableHeaderColumn>
+            <TableHeaderColumn dataField='name'>
+              Your language
+            </TableHeaderColumn>
+            <TableHeaderColumn dataField='value'>
+              Translate
+            </TableHeaderColumn>
+          </BootstrapTable>
+        </div>
+      );
+    }
+  }
+   
+  export default Table1;

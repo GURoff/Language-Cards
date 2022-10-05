bfccb7dc4599f95324d37f80012b576a767e8bce
Alexandr <alexandrgurov1@gmail.com>
2022-10-05 17:02:31 +0200

    created table for testing

---
+++ src/components/TestVocabularyTableHeader.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
+import '../styles/TestVocabularyTable.css';
+import '../../node_modules/react-bootstrap-table/css/react-bootstrap-table.css';
+ 
+ 
+class TestVocabularyTableHeader extends Component {
+  render(props) {
+    return (
+      <div>
+        <BootstrapTable data={props}>
+          <TableHeaderColumn isKey dataField='props.number'>
+            ID
+          </TableHeaderColumn>
+          <TableHeaderColumn dataField='props.word.input'>
+            Name
+          </TableHeaderColumn>
+          <TableHeaderColumn dataField='props.word.output'>
+            Value
+          </TableHeaderColumn>
+        </BootstrapTable>
+      </div>
+    );
+  }
+}
+ 
+export default TestVocabularyTableHeader;
+
+
+

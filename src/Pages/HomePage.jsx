3f7aafdb4de653370f37bbddf6acf0bdebef0452
Alexandr <alexandrgurov1@gmail.com>
2022-10-06 14:43:28 +0200

    created Home page

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1 style={{ textAlign: "center" }}>Homepage</h1>
+            <hr style={{ margin: '10px 0' }} />
+        </div>
+    );
+};
+
+export default HomePage;

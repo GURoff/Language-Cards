0f13a7e3b9ad0f0978882fd60621e74dff8dd97a
Alexandr <alexandrgurov1@gmail.com>
2022-10-06 14:43:03 +0200

    created page About

---
+++ src/Pages/About.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1 style={{ textAlign: "center" }}>ABOUT</h1>
+            <hr style={{ margin: '10px 0' }} />
+            <p style={{ color: 'black', textAlign: "center" }}>
+                This application was created as part of the training and
+                will be used as a portfolio project in the future.
+            </p>
+        </div>
+    );
+};
+
+export default About;

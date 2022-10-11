db77f644fb1c43b25de57ac4ee028f16992eadd3
Alexandr <alexandrgurov1@gmail.com>
2022-10-11 11:54:21 +0200

    added 1 cooment for Routes for future

--- src/AppAlexandr.js
+++ src/AppAlexandr.js
@@ -4,6 +4,7 @@ import Navbar from './components/UI/Navbar/Navbar';
 import HomePage from './Pages/HomePage';
 import Translator from "./Pages/API/Translator";
 import About from './Pages/About';
+import { routes } from "./Router/routes";
 
 import './styles/App.css';
 
@@ -15,6 +16,11 @@ function AppAlexandr() {
       <BrowserRouter>
      <Navbar/>
         <Routes>
+          {/* {routes.map(route => 
+            <Route path = {route.path}
+            element = {route.element}
+            exact = {route.exact}/>
+          )} */}
           <Route path="/home" element={<HomePage/>}/>
           <Route path="/vocabulary" element={<Translator/>}/>
           <Route path="/about" element={<About/>}/>

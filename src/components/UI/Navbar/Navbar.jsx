c8342a791a6956a7d57cf6acc7bfc8e498016a72
Alexandr <alexandrgurov1@gmail.com>
2023-02-28 21:32:02 +0100

    added new navbar for testing 28.02.23

--- src/components/UI/Navbar/Navbar.jsx
+++ src/components/UI/Navbar/Navbar.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 
-const Navbar = () => {
+const NavbarModern = () => {
     return (
+        //NavbarModern
+        // <div>
+        //     <nav class="menu">
+
+        //         <input type="radio" name="nav-item" id="m-home" checked /><label htmlFor="m-home">Home</label>
+        //         <input type="radio" name="nav-item" id="m-search" /><label htmlFor="m-search">Search</label>
+        //         <input type="radio" name="nav-item" id="m-notification" /><label htmlFor="m-notification">Notification</label>
+        //         <input type="radio" name="nav-item" id="m-favorites" /><label htmlFor="m-favorites">Favorites</label>
+        //         <input type="radio" name="nav-item" id="m-profile" /><label htmlFor="m-profile">Profile</label>
+
+        //         <div class="selector"></div>
+        //     </nav>
+        // </div>
+
+        //Old Navbar
+
         <div className='navbar'>
             <div className='navbar__links'>
                 <Link to='/home'>Home</Link>
@@ -12,5 +28,5 @@ const Navbar = () => {
         </div>
     );
 };
-
-export default Navbar;
+//export default Navbar;
+export default NavbarModern;

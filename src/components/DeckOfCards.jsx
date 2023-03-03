2f4f3928ace9883e709c2fa459f45666d51235b0
Alexandr <alexandrgurov1@gmail.com>
2023-03-03 22:22:11 +0100

    created a new file for deck of cards

---
+++ src/components/DeckOfCards.jsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import '../styles/DeckOfCards.css';
+
+const Deck = ({ cards }) => {
+    const [deck, setDeck] = useState(cards);
+
+    useEffect(() => {
+        setDeck(cards);
+    }, [cards]);
+
+    const flipTopCard = () => {
+        setDeck(prevDeck => {
+            const newDeck = prevDeck.slice();
+            newDeck.reverse();
+            newDeck[0].isFlipped = !newDeck[0].isFlipped;
+            return newDeck.reverse();
+        });
+    };
+
+    const addCard = card => {
+        setDeck(prevDeck => prevDeck.concat(card));
+    };
+
+    return (
+        <div className="deck-container">
+            {deck.map((card, index) => (
+                <div
+                    className={`card-container ${card.isFlipped ? 'is-flipped' : ''}`}
+                    key={index}
+                    style={{ zIndex: deck.length - index }}
+                >
+                    <div className="card-face card-front">
+                        {card.frontContent}
+                    </div>
+                    <div className="card-face card-back">
+                        {card.backContent}
+                    </div>
+                </div>
+            ))}
+            <button onClick={flipTopCard}>Flip Top Card</button>
+            <button onClick={() => addCard({ frontContent: 'New Card', backContent: 'New Card' })}>Add Card</button>
+        </div>
+    );
+};
+
+export default Deck;

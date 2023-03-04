3ca9fd66105ea754b78455e01d5f9e9665b28786
Alexandr <alexandrgurov1@gmail.com>
2023-03-04 22:55:14 +0100

    without changes

--- src/Router/routes.js
+++ src/Router/routes.js
@@ -3,18 +3,18 @@ import Translator from "../Pages/API/Translator";
 import About from "../Pages/About";
 
 export const routes = [{
-        path: '/home',
-        element: HomePage,
-        exact: true
-    },
-    {
-        path: '/vocabulary',
-        element: Translator,
-        exact: true
-    },
-    {
-        path: '/about',
-        element: About,
-        exact: true
-    },
+    path: '/home',
+    element: HomePage,
+    exact: true
+},
+{
+    path: '/vocabulary',
+    element: Translator,
+    exact: true
+},
+{
+    path: '/about',
+    element: About,
+    exact: true
+},
 ]

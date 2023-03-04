5c70cdc7129d2f42a3678e431af18f86b007fdc0
Alexandr <alexandrgurov1@gmail.com>
2023-03-04 22:54:31 +0100

    process of testing

--- src/Pages/TestPage.jsx
+++ src/Pages/TestPage.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
-import Card from '../components/Card';
+import React, { useState } from 'react';
+import VocabularyItem from '../components/VocabularyItem';
+import Cards from '../components/Cards';
 import DeckOfCards from '../components/DeckOfCards';
 
 const TestPage = () => {
+    const [frontContent, setFrontContent] = useState('');
+    const [backContent, setBackContent] = useState('');
+
     return (
         <div>
-            <Card />
+            {/* <Cards frontContent={frontContent}
+                backContent={backContent} /> */}
             {/* <DeckOfCards /> */}
         </div>
     );

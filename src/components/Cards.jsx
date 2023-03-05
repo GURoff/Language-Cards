67a8bcbce7032825dc1efa72eb9d6de6244360c4
Alexandr <alexandrgurov1@gmail.com>
2023-03-05 22:18:46 +0100

    deleted errors

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import '../styles/Cards.css';
-import VocabularyItem from './VocabularyItem';
+
 // { frontContent, backContent }
-const Card = (props) => {
+const Cards = (props) => {
     const [isFlipped, setIsFlipped] = useState(false);
 
     const handleClick = () => {
@@ -11,20 +11,24 @@ const Card = (props) => {
     // onClick={handleClick}
     return (
         <div className={`card ${isFlipped ? 'flipped' : ''}`} onClick={handleClick}>
+            <div>
+
+            </div>
             <div className="front">
                 <div className="textOnCard">
-                    <h2>{props.word.input}</h2>
+                    <h2>front</h2>
                     <p>RU</p>
                 </div>
             </div>
             <div className="back">
                 <div className="textOnCard">
-                    <h2>{props.word.output}</h2>
+                    <h2>back</h2>
                     <p>Eng</p>
                 </div>
             </div>
+
         </div>
     );
 };
 
-export default Card;
+export default Cards;
